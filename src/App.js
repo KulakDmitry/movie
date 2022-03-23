6e6bf9396253ddab92d4bc93dad0889489f4d0ce
KulakDmitry <kulakdima@mail.ru>
2022-03-23 17:22:50 +0300

    update routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import HomePage from "./components/Homepage/HomePage";
 import MoviePage from "./components/Moviepage/MoviePage";
 
 function App() {
-  const [movieInfo, setMovieInfo] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<HomePage setMovieInfo={setMovieInfo} />} />
-        <Route path="movie/:id" element={<MoviePage movieInfo={movieInfo} />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={<HomePage isLoading={isLoading} setIsLoading={setIsLoading} />}
+      />
+      <Route
+        path="/movie/:id"
+        element={
+          <MoviePage isLoading={isLoading} setIsLoading={setIsLoading} />
+        }
+      />
+      <Route
+        path="/page/:page/movie/:id"
+        element={
+          <MoviePage isLoading={isLoading} setIsLoading={setIsLoading} />
+        }
+      />
+      <Route
+        path="/page/:page"
+        element={<HomePage isLoading={isLoading} setIsLoading={setIsLoading} />}
+      ></Route>
+    </Routes>
   );
 }
 
